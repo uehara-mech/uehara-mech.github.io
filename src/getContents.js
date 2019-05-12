b0e73d4eea543bcfde21f97dd66b7257b068c734
uehara-mech <starlight00731@gmail.com>
2019-05-12 23:45:25 +0900

    [update] change url

--- src/getContents.js
+++ src/getContents.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 async function getContents() {
     console.log('getContents');
     try {
-        return (await axios.get('https://raw.githubusercontent.com/uehara-mech/uehara-mech.github.io/gh_pages/contents.yaml?token=AFORA7NYVXYCHXDUSVCICFK44FSUS', 
+        return (await axios.get('https://raw.githubusercontent.com/uehara-mech/gh_pages/master/contents.yaml', 
         )).data;
     } catch (error) {
         console.log(error);
